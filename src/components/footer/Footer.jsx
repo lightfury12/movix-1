3b3580fddcfb642174c36e2daaddbe056e188ba5
aruntaak123 <av932105@gmail.com>
2023-11-19 16:36:13 +0530

    Problem Fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
 } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
@@ -11,45 +10,45 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import "./style.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <a href="https://instagram.com/arun_taak_123">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                        </a>
-                    <a href="https://instagram.com/arun_taak_123">
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                        </a>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          <h1> 	&#128578; &#128522; <span> Powered by Arun Taak </span> &#128522; 	&#128578;</h1><br/>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
+        <div className="socialIcons">
+          <a href="https://instagram.com/aruntaak36">
+            <span className="icon">
+              <FaFacebookF />
+            </span>
+          </a>
+          <a href="https://instagram.com/__aru__xn__">
+            <span className="icon">
+              <FaInstagram />
+            </span>
+          </a>
+          <a href="https://twitter.com/Aruntaak22">
+            <span className="icon">
+              <FaTwitter />
+            </span>
+          </a>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
